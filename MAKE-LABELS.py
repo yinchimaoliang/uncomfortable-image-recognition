c28088ece906fa8b3c6a3e09576fe8a4e9c88d1f
Yinhao Li <liyinhao0413@qq.com>
2019-05-21 19:09:08 +0800

    finish make labels

--- MAKE-LABELS.py
+++ MAKE-LABELS.py
@@ -19,13 +19,21 @@ class main():
 
         for fold_name in self.folds:
             path = DATASET_PATH + '/' + fold_name
+            print(path)
+            self.data.append(['.' + path[6:] + '/' + i for i in os.listdir(path)])
 
-            self.data.append([path + '/' + i for i in os.listdir(path)])
-
-
+    def makeLabels(self):
+        with open(TRAIN_PATH,'w') as f:
+            for i in range(len(self.data)):
+                for j in range(len(self.data[i])):
+                    f.write(self.data[i][j] + ' ' + str(i) + '\n')
+                    # f.write()
+            # pass
 
     def mainFunc(self):
         self.readData()
+        # print(self.data)
+        self.makeLabels()
         # for i, j, k in os.walk(self.dataset_path):
         #     print(i, j, k)
 
